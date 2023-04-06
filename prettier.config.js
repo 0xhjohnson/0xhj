4ebd152e3e93d80e5c1ea7eb5b5dcd782f7e1459
Hunter Johnson <0xhjohnson@protonmail.com>
2023-04-05 21:51:43 -0400

    chore: fix formatting using prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  singleQuote: true,
-  goTemplateBracketSpacing: true,
-  bracketSameLine: false,
-  overrides: [
-    {
-      files: ['*.html'],
-      options: {
-        parser: 'go-template',
-      },
-    },
-  ],
-  plugins: [require.resolve('prettier-plugin-go-template')],
-};
+	singleQuote: true,
+	useTabs: true,
+	semi: false,
+	goTemplateBracketSpacing: true,
+	overrides: [
+		{
+			files: ['*.html'],
+			options: {
+				parser: 'go-template',
+			},
+		},
+	],
+	plugins: [require.resolve('prettier-plugin-go-template')],
+}
